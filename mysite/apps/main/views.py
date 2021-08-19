ca2aaf2548f3b2b482de0da7878f7d174e601cfd
26huang <weishuoh@gmail.com>
2021-08-18 21:30:33 -0400

    updated home page to loop through Subject data

--- mysite/apps/main/views.py
+++ mysite/apps/main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Subject
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Hello World!')
+    # return HttpResponse('Hello World!')
+    return render(request=request, template_name="main/home.html", context={'subject': Subject.objects.all})
